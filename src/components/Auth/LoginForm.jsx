9fa2eb2c5cf58b2d0052c00d421725a25b7d85c2
TrulyMahra <mahraais653@gmail.com>
2024-03-25 20:02:42 +0530

    createpost backend connection works now

--- src/components/Auth/LoginForm.jsx
+++ src/components/Auth/LoginForm.jsx
@@ -5,6 +5,7 @@ const LoginForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  const [isVerified, setIsVerified] = useState(false);
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
@@ -17,76 +18,83 @@ const LoginForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-        const response = await fetch('http://localhost:3001/api/v1/login', {
-            method:'POST',
-            headers:{
-                'Content-Type': 'application/json',
-            },
-            body:JSON.stringify({
-                email,
-                password
-            }),
-        });
-        const responseData = await response.json();
-        if(response.ok) {
-            console.log('user logged in successfully:', responseData);
-            window.location.href ='/'
-        }else {
-            console.error('User login failed:', responseData);
-            setError('Invalid email or password');
-        }
+      const response = await fetch('http://localhost:3001/api/v1/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email,
+          password
+        }),
+      });
+      const responseData = await response.json();
+      console.log('Response Data:', responseData);
+      if (response.ok) {
+        console.log('user logged in successfully:', responseData);
+         localStorage.setItem('token', responseData.data.token);
+         setIsVerified(true);
+        //  window.location.href = '/'
+      } else {
+        console.error('User login failed:', responseData);
+        setError('Invalid email or password');
+      }
     } catch (error) {
-        console.error('Error:', error);
-        setError('An error occurred. Please try again later.');
+      console.error('Error:', error);
+      setError('An error occurred. Please try again later.');
     }
     console.log({
-        email,
-        password
+      email,
+      password
     })
   };
+  
+  if(isVerified) {
+    return window.location.href = '/'
+  }
 
   return (
     <Center h="90vh">
-        <Box maxW="lg" bg="gray.600" rounded="lg" p={10}>
+      <Box maxW="lg" bg="gray.600" rounded="lg" p={10}>
 
-    <Box maxW="md" bg="gray.800" rounded="lg" p={6}>
-      <form onSubmit={handleSubmit}>
-        <FormControl mb={4}>
-          <FormLabel htmlFor="email">Email</FormLabel>
-          <Input
-            id="email"
-            type="email"
-            placeholder="Your email"
-            value={email}
-            onChange={handleEmailChange}
-            />
-        </FormControl>
-        <FormControl mb={6}>
-          <FormLabel htmlFor="password">Password</FormLabel>
-          <Input
-            id="password"
-            type="password"
-            placeholder="Your password"
-            value={password}
-            onChange={handlePasswordChange}
-            />
-        </FormControl>
-        {error && <p color="red.500" fontSize="xs" fontStyle="italic">{error}</p>}
-        <Link to="/">
-        <Button colorScheme="blue" type="submit">
-          Login
-        </Button>
-        </Link>
-      </form>
-      <Box mt={4} textAlign="center">
-        <p>Not have an account? </p>
-        <Link to='/register' style={{ color: 'teal' }}>
-          Register here
-        </Link>
+        <Box maxW="md" bg="gray.800" rounded="lg" p={6}>
+          <form onSubmit={handleSubmit}>
+            <FormControl mb={4}>
+              <FormLabel htmlFor="email">Email</FormLabel>
+              <Input
+                id="email"
+                type="email"
+                placeholder="Your email"
+                value={email}
+                onChange={handleEmailChange}
+                required
+              />
+            </FormControl>
+            <FormControl mb={6}>
+              <FormLabel htmlFor="password">Password</FormLabel>
+              <Input
+                id="password"
+                type="password"
+                placeholder="Your password"
+                value={password}
+                onChange={handlePasswordChange}
+                required
+              />
+            </FormControl>
+            {error && <p color="red.500" fontSize="xs" fontStyle="italic">{error}</p>}
+              <Button colorScheme="blue" type="submit">
+                Login
+              </Button>
+          </form>
+          <Box mt={4} textAlign="center">
+            <p>Not have an account? </p>
+            <Link to='/register' style={{ color: 'teal' }}>
+              Register here
+            </Link>
+          </Box>
+        </Box>
       </Box>
-    </Box>
-            </Box>
-  </Center>
+    </Center>
   );
 };
 
--- src/pages/CreatePostPage/CreatePostPage.jsx
+++ src/pages/CreatePostPage/CreatePostPage.jsx
@@ -1,40 +1,95 @@
+import { useState } from 'react'
 import Sidebar from "../../components/Sidebar/Sidebar";
 import Nav from "../../components/Navbar/Nav";
-import {Textarea,Select, Input, Button, Box, Center } from "@chakra-ui/react";
+import { Textarea, Select, Input, Button, Box, Center } from "@chakra-ui/react";
 const CreatePostPage = () => {
+
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [pincode, setPincode] = useState('');
+  const [sports, setSports] = useState('');
+
+  function handleTitleChange(event) {
+    setTitle(event.target.value)
+  }
+
+  function handleDescriptionChange(event) {
+    setDescription(event.target.value)
+  }
+
+  function handlePinCodeChange(event) {
+    setPincode(event.target.value)
+  }
+
+  function handleSportsChange(event) {
+    setSports(event.target.value)
+  }
+
+
+  const handlePostSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const token = localStorage.getItem('token');
+      const response = await fetch('http://localhost:3001/api/v1/post', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `${token}`
+        },
+        body: JSON.stringify({ title, description, pincode, sports }),
+      });
+      const responseData = await response.json();
+      if (response.ok) {
+        console.log('post created successfully', responseData);
+      } else {
+        console.error('post creation failed', responseData)
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+    console.log({
+      title,
+      description,
+      pincode,
+      sports
+    })
+  }
+
   return (
     <>
-    <Nav />
-    <>
-      <Sidebar />
-      <Center h="80vh">
-    <Box maxW="md" bg={"black"} border={"1px solid gray"} mt={10}  rounded="lg" p={6}>
-    <Input
+        <Nav />
+      <>
+        <Sidebar />
+        <Center h="80vh">
+          <Box as="form" maxW="md" bg={"black"} border={"1px solid gray"} mt={10} rounded="lg" p={6} onSubmit={handlePostSubmit}>
+            <Input
               type="file"
               id="avatar"
               name="avatar"
               pt={1}
               accept="image/png, image/jpeg"
             />
-            <Input placeholder="Pin Code" mt={5} />
+            <Input placeholder="Pin Code" mt={5} value={pincode} onChange={handlePinCodeChange} />
 
-            <Input placeholder="Title" mt={5} />
+            <Input placeholder="Title" mt={5} value={title} onChange={handleTitleChange} />
 
-            <Textarea placeholder="Description..." mt={5} />
-            <Select placeholder="Select option" mt={5}>
-              <option value="option1">Cricket</option>
-              <option value="option2">Volleyball</option>
-              <option value="option3">Table Tennis</option>
+            <Textarea placeholder="Description..." mt={5} value={description} onChange={handleDescriptionChange} />
+            <Select placeholder="Select option" mt={5} value={sports} onChange={handleSportsChange}>
+              <option value="cricket">Cricket</option>
+              <option value="volleyball">Volleyball</option>
+              <option value="TableTennis">Table Tennis</option>
+              <option value="badminton">Badminton</option>
+              <option value="ArmWrestling">Arm Wrestling</option>
             </Select>
 
             <Input type="file" hidden />
-      <Box mt={4} textAlign="center">
-      <Button mr={3}>Post</Button>
-      </Box>
-    </Box>
-  </Center>
+            <Box mt={4} textAlign="center">
+              <Button type="submit" mr={3}>Post</Button>
+            </Box>
+          </Box>
+        </Center>
+      </>
     </>
-  </>
   )
 }
 
